1aa22c6ac1faa5cd5c9b15a91c58ae3d14fc8bb3
jk21635 <joshrkulp@gmail.com>
2013-02-18 16:44:51 -0500

    Code Sample

---
+++ ModernMedical/ModernMedical/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernMedical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int i = 1 ;i < 101; i++)
+            {
+                string strOutput = "";
+                bool bShowNumber = true;
+                if (i % 3 == 0)
+                {
+                    strOutput = "Modern";
+                    bShowNumber = false;
+                }
+                if (i % 5 == 0)
+                {
+                    strOutput += "Medical";
+                    bShowNumber = false;
+                }
+                if(bShowNumber)
+                    strOutput = i.ToString();
+                Console.WriteLine(strOutput);
+            }
+           Console.ReadLine();
+        }
+    }
+}
